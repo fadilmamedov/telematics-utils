2b5c5b754ae7b7f15c837daa3787cbfd3e3002eb
Fadil Mamedov <fadil.mamedov@gmail.com>
2022-05-30 15:48:32 +0300

    Implement navigation logs generator

--- scripts/generateNavigationLogs/index.js
+++ scripts/generateNavigationLogs/index.js
@@ -1,5 +1,7 @@
 const fs = require("fs");
+const moment = require("moment");
 const commandLineArgs = require("command-line-args");
+const papaparse = require("papaparse");
 
 const {
   source: sourceFile,
@@ -14,5 +16,52 @@ const {
 const sourceFileContent = fs.readFileSync(sourceFile, "utf-8");
 
 const vehicleStats = JSON.parse(sourceFileContent);
+const vehicleEngineStateStats = vehicleStats.filter((s) => s.type === "engine");
+const vehicleGpsStats = vehicleStats.filter((s) => s.type === "gps");
 
-console.log(fetchIntervalInSeconds);
+const startDate = moment(vehicleStats[0].date);
+const endDate = moment(vehicleStats[vehicleStats.length - 1].date).add(
+  fetchIntervalInSeconds,
+  "seconds"
+);
+
+let id = 1;
+let currentDate = moment(startDate);
+
+const navigationLogs = [];
+
+while (currentDate.toDate() < endDate.toDate()) {
+  const engine = findVehicleStatsBeforeDate(vehicleEngineStateStats, currentDate.toDate());
+  const gps = findVehicleStatsBeforeDate(vehicleGpsStats, currentDate.toDate());
+
+  if (!engine || !gps) continue;
+
+  navigationLogs.push({
+    id,
+    device_id: "DEVICE_ID",
+    vehicle_id: "f1ed2cf8-9cea-4347-9946-eb2f893d0509",
+    engine_state_date: engine.date,
+    engine_state: engine.value,
+    gps_date: gps.date,
+    gps_lng: gps.location[0],
+    gps_lat: gps.location[1],
+    gps_formatted_location: gps.formattedLocation,
+    request_date: currentDate.utc().format(),
+  });
+
+  id++;
+  currentDate.add(fetchIntervalInSeconds, "seconds");
+}
+
+const navigationLogsCSV = papaparse.unparse(navigationLogs);
+fs.writeFileSync(outputFile, navigationLogsCSV, "utf-8");
+
+function findVehicleStatsBeforeDate(vehicleStats, date) {
+  let i = 0;
+  try {
+    while (vehicleStats[i] && moment(vehicleStats[i].date).toDate() <= currentDate) i++;
+    return vehicleStats[i - 1];
+  } catch {
+    console.log(i);
+  }
+}
